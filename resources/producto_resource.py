384ad40a48815f35a07f03a8cc427b626738e20a
Jesusbarah <66099847+Jesusbarah@users.noreply.github.com>
2021-08-02 19:16:15 -0600

    Updating resources to apply changes

--- resources/producto_resource.py
+++ resources/producto_resource.py
@@ -4,23 +4,23 @@ from logic.producto_logic import ProductoLogic
 
 class Producto(Resource):
     def __init__(self):
-        self.producto_put_args = self.createParser()
+        #self.producto_put_args = self.createParser()
         self.logic = ProductoLogic()
 
-    def createParser(self):
+    """def createParser(self):
         args = reqparse.RequestParser()
         args.add_argument("Nombre", type=str, help="Nombre del producto")
         args.add_argument("Categoria", type=str, help="Categoria del producto")
         args.add_argument("Precio", type=float, help="Precio del producto")
         args.add_argument("Imagen", type=str, help="Imagen del producto")
-        return args
+        return args"""
 
+        
     def get(self, id):
-        result = self.logic.getProductoById(id)
-        return result, 200
-
-
-    def post(self, id):
         productoDict = self.logic.getAllProductos()
         result = productoDict
+        return result, 200
+    
+    def post(self, id):
+        result = self.logic.getProductoById(id)
         return result, 200
